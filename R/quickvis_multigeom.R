f8b39496c69a42bc6873356869f653d1a4b982e3
AndreasWei <andix.weiss04@gmail.com>
2025-07-22 00:13:27 +0200

    Added documentation to both functions

--- R/quickvis_multigeom.R
+++ R/quickvis_multigeom.R
@@ -1,25 +1,36 @@
-quickvis_mulitgeom <- function (data = NULL, geom1 = c("", "", ""), geom2 = c("", "", ""), geom3 = c("", "", "")) {
+#' Allows to use multiple different geoms with different aesthetic mapping. Intended to just give an first impression how the different geoms would look as a plot. Lacks functionality compared to using the normal ggplot function. For a bit more functionality use [QuickVis::quickvis_simple()] and add further geoms after the function.
+#'
+#' @param data Default dataset to use for plot.
+#' @param geom1 First of 3 geoms available to add. Plot as follows "c ("Mapping for x-axis", "Mapping for y- Axis", "geom").
+#' @param geom2 Plot as follows "c ("Mapping for x-axis", "Mapping for y- Axis", "geom").
+#' @param geom3 Plot as follows "c ("Mapping for x-axis", "Mapping for y- Axis", "geom").
+#'
+#' @returns ggplot
+#' @export
+#'
+#' @examples quickvis_mulitgeom(mtcars, c ("hp", "cyl", "point"), c ("hp", "cyl", "line"))
+quickvis_mulitgeom <- function (data = NULL, geom1 = c("x", "y", "geom"), geom2 = c("x", "y", "geom"), geom3 = c("x", "y", "geom")) {
 
   # Create plot
   plot <- ggplot2::ggplot(data)
 
 
   # Create geom1
-  if (all(geom1 != c("", "", ""))) {
+  if (all(geom1 != c("x", "y", "geom"))) {
     plot <- plot + (get(paste0("geom_", geom1[3]), envir = asNamespace("ggplot2")))(ggplot2::aes_string(
       x = geom1[1],
       y = geom1[2]
     ))
   }
   # Create geom2
-  if (all(geom2 != c("", "", ""))) {
+  if (all(geom2 != c("x", "y", "geom"))) {
     plot <- plot + (get(paste0("geom_", geom2[3]), envir = asNamespace("ggplot2")))(ggplot2::aes_string(
       x = geom2[1],
       y = geom2[2]
     ))
   }
   # Create geom3
-  if (all(geom3 != c("", "", ""))) {
+  if (all(geom3 != c("x", "y", "geom"))) {
     plot <- plot + (get(paste0("geom_", geom3[3]), envir = asNamespace("ggplot2")))(ggplot2::aes_string(
       x = geom3[1],
       y = geom3[2]
--- R/quickvis_simple.R
+++ R/quickvis_simple.R
@@ -1,16 +1,32 @@
-#' Create point plot quickly.
+#' Useful to create a simple plot quickly. Uses ggplot to create the plot. Alternative to ggplot if you want to create a simple plot to visualise data quicker with less complex code. Allows to add more ggplot functions.
 #'
-#' @param data dataset
-#' @param x x-axis parameter
-#' @param y y-axis parameter
-#' @param theme decide theme
-#' @param facet_wrap decide if facetwrap should be enabled
-#' @param col lets you adjust color of geom based on variable in the dataset
+#' @param data Default dataset to use for plot.
+#' @param x Variable which will be mapped to the x-axis of the plot.
+#' @param y Variable which will be mapped to the y-axis of the plot.
+#' @param geom Decide geom used no geom will result in the usage of [ggplot2::geom_point()].
+#' @param theme Lets you decide which theme you want to use. No theme will be applied if left empty.
+#' @param facet_wrap Decide which variable you want to use for a facet_wrap. When left empty there will be no facet wrap.
+#' @param col Decides variable of dataset which will color the geom. Not colors will be applied if left empty.
 #'
 #' @returns ggplot
 #' @export
 #'
-#' @examples quickvis_simple(mtcars, "mpg", "cyl")
+#' @examples quickvis_simple(mtcars, "hp", "cyl")
+#' @examples quickvis_simple(mtcars, "hp", geom = "bar")
+#' @examples quickvis_simple(mtcars, "mpg", "cyl", "line", "bw")
+#'
+#' # using facet_wrap argument
+#' @examples quickvis_simple(mtcars, "cyl", "hp", facet_wrap = "gear")
+#'
+#' # using col argument
+#' @examples quickvis_simple(mtcars, "gear", "hp", col = "vs")
+#'
+#' # U can also add other ggplot2 functions after the function to further enhance the plot
+#' @examples quickvis_simple(mtcars, "wt", "cyl", "point", "minimal") +
+#' geom_line() +
+#' labs(title = "Title", x = "X", y = "Y")
+#'
+#'
 quickvis_simple <- function(data = NULL, x, y, geom = "point", theme = "none", facet_wrap = "F", col = NULL) {
   # Create plot
   plot <- ggplot2::ggplot(data, ggplot2::aes_string(x, y,col = col))
